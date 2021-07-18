d6ac67e8fd9c4db2b751cd16567fd14df60855bd
verstunio <vladchegzt@gmail.com>
2021-07-18 23:19:56 +0300

    added model to particles lesson

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'dat.gui'
 import { Scene } from 'three'
 
@@ -15,30 +16,78 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-// scene.background = new THREE.Color( 'white' );
+
+/**
+ * Models
+ */
+
+ const gltfLoader = new GLTFLoader()
+let model
+ gltfLoader.load(
+    '/models/astrodoggo/scene.gltf',
+    (gltf) =>
+    {
+        const children = [...gltf.scene.children]
+        for(const child of children)
+        {
+            model = gltf.scene;
+            child.rotateZ(-2.5);
+            scene.add(child)
+            console.log(child)
+        }
+
+        // scene.add(gltf.scene)
+    }
+)
+
+
+/**
+ * Lights
+ */
+ const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+ scene.add(ambientLight)
+ 
+ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+ directionalLight.castShadow = true
+ directionalLight.shadow.mapSize.set(1024, 1024)
+ directionalLight.shadow.camera.far = 15
+ directionalLight.shadow.camera.left = - 7
+ directionalLight.shadow.camera.top = 7
+ directionalLight.shadow.camera.right = 7
+ directionalLight.shadow.camera.bottom = - 7
+ directionalLight.position.set(5, 5, 5)
+ scene.add(directionalLight)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const partcielsTexture = textureLoader.load('/textures/particles/2.png')
+const partcielsTexture = textureLoader.load('/textures/particles/9.png')
 
 /**
  * Particles
  */
 //Geometry
 const particlesGeometry = new THREE.BufferGeometry()
-const count = 50000
+const count = 5000
 
 const positions = new Float32Array( count * 3)
+const colors = new Float32Array( count * 3)
+
 for (let i = 0; i < count * 3; i++) 
 {
     positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
 }
 
 particlesGeometry.setAttribute(
     'position',
     new THREE.BufferAttribute(positions, 3)
+)
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
 )
 
 //Material
@@ -53,6 +102,7 @@ particlesMaterial.alphaMap = partcielsTexture
 // particlesMaterial.depthTest = false
 particlesMaterial.depthWrite = false
 particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
 
 //Points
@@ -63,7 +113,7 @@ const cube = new THREE.Mesh(
     new THREE.BoxGeometry(),
     new THREE.MeshBasicMaterial()
 )
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
@@ -117,6 +167,19 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update particles
+    particles.rotation.y = elapsedTime * 0.6
+
+    // cube.rotation.y = elapsedTime * 0.2
+    // for(let i = 0; i < count; i++)
+    // {
+    //     let i3 = i * 3
+
+    //     const x = particlesGeometry.attributes.position.array[i3]
+    //     particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    // }
+    // particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
